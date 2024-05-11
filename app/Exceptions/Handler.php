440896bcb7a983cbbee23853c3dc9cf90b6aeca2
EliiDaniel <monteroso.eli@gmail.com>
2024-05-11 20:07:41 +0800

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,6 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Session\TokenMismatchException; 
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -27,21 +26,5 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     * @return \Illuminate\Http\Response
-     *
-     * @throws \Throwable
-     */
-    public function render($request, Throwable $exception)
-    {
-        if ($exception instanceof TokenMismatchException) {
-            return redirect('/');
-        }
     }
 }
